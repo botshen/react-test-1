db385eb05202e711812c1fe869878aed22c9f010
shenxin <botzgbj@gmail.com>
2022-12-22 15:31:14 +0800

    注册的基本布局的测试

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,10 +1,21 @@
-const SignUpPage = () => {
-  return (
-        <div>
-            <h1>Sign Up</h1>
-            <input />
-        </div>
-  )
-}
+import { Component } from 'react'
 
+class SignUpPage extends Component {
+  render() {
+    return (
+            <div>
+                <h1>Sign Up</h1>
+                <label htmlFor='username'>Username</label>
+                <input id='username' />
+                <label htmlFor='email'>E-mail</label>
+                <input id='email' />
+                <label htmlFor='password'>Password</label>
+                <input id='password' type='password' />
+                <label htmlFor='passwordRepeat'>Password Repeat</label>
+                <input id='passwordRepeat' type='password' />
+                <button disabled>Sign up</button>
+            </div>
+    )
+  }
+}
 export default SignUpPage
--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -9,9 +9,39 @@ describe('Sign Up Page', () => {
       expect(header).toBeInTheDocument()
     })
     it('has username input', () => {
-      const { container } = render(<SignUpPage />)
-      const input = container.querySelector('input')
+      render(<SignUpPage />)
+      const input = screen.getByLabelText('Username')
       expect(input).toBeInTheDocument()
+    })
+    it('has email input', () => {
+      render(<SignUpPage />)
+      const input = screen.getByLabelText('E-mail')
+      expect(input).toBeInTheDocument()
+    })
+    it('has password input', () => {
+      render(<SignUpPage />)
+      const input = screen.getByLabelText('Password')
+      expect(input).toBeInTheDocument()
+    })
+    it('has password type for password input', () => {
+      render(<SignUpPage />)
+      const input = screen.getByLabelText('Password')
+      expect(input.type).toBe('password')
+    })
+    it('has password type for password repeat input', () => {
+      render(<SignUpPage />)
+      const input = screen.getByLabelText('Password Repeat')
+      expect(input.type).toBe('password')
+    })
+    it('has SignUp button', () => {
+      render(<SignUpPage />)
+      const button = screen.queryByRole('button', { name: 'Sign up' })
+      expect(button).toBeInTheDocument()
+    })
+    it('disables the button initially', () => {
+      render(<SignUpPage />)
+      const button = screen.queryByRole('button', { name: 'Sign up' })
+      expect(button).toBeDisabled()
     })
   })
 })
